276e67c2b65c6c88e2d72e3b16c94d72e8428b64
Furkan Cebe <f.cebe.2000@gmail.com>
2025-03-10 23:17:35 +0300

    files added

---
+++ SQLQuery1.sql
@@ -0,0 +1,70 @@
+--Veritabaný adý Müziksel
+
+--CREATE TABLE sanatci(
+--SanatçýAdý varchar(15),
+--SanatçýSoyadý varchar(15),
+--MüzikTürü varchar(15),
+--DoðumTarihi date,
+--Boyu int,
+--PRIMARY KEY (SanatçýAdý, SanatçýSoyadý, DoðumTarihi));
+
+--CREATE TABLE muzikTuru(
+--TürünAdý varchar(15),
+--TürünTercihEdilmeYüzdesi int,
+--TürüTercihEdenlerinYaþAralýðý varchar(15),
+--PRIMARY KEY (TürünAdý));
+
+--CREATE TABLE yasAraligi(
+--YaþAralýðý varchar(15),
+--TürAdý varchar(15),
+--Bölgesi varchar(40),
+--CinsiyetOraný varchar(15),
+--PRIMARY KEY (YaþAralýðý));
+
+
+
+
+--INSERT INTO sanatci VALUES ('Mustafa', 'Ceceli', 'Pop', '1980-11-02', 174);
+--INSERT INTO sanatci VALUES ('Sinan', 'Akçýl', 'Pop', '1982-05-20', 178);
+--INSERT INTO sanatci VALUES ('Ahmet', 'Kaya', 'Arabesk', '1957-10-28', 180);
+--INSERT INTO sanatci VALUES ('Demet', 'Akalýn', 'Pop', '1972-04-23', 173);
+--INSERT INTO sanatci VALUES ('Ali', 'Kýnýk', 'Türk Halk', '1974-09-26', 175);
+--INSERT INTO sanatci VALUES ('Alperen', 'Kekilli', 'Türk Halk', '1980-01-01', 173);
+--INSERT INTO sanatci VALUES ('Orhan', 'Gencebay', 'Arabesk', '1944-08-04', 173);
+--INSERT INTO sanatci VALUES ('Selda', 'Baðcan', 'Türk Halk', '1948-12-14', 157);
+--INSERT INTO sanatci VALUES ('Cenk', 'Baþ', 'Pop', '1998-04-14', 186);
+--INSERT INTO sanatci VALUES ('Abdullah', 'Evgi', 'Rap', '1991-02-16', 171);
+--INSERT INTO sanatci VALUES ('Ahmet', 'Þafak', 'Türk Halk', '1962-01-08', 170);
+--INSERT INTO sanatci VALUES ('Sibel', 'Can', 'Pop', '1970-08-01', 169);
+--INSERT INTO sanatci VALUES ('Murat', 'Belet', 'Ýlahi', '1985-04-13', 175);
+--INSERT INTO sanatci VALUES ('Muazzez', 'Ersoy', 'Arabesk', '1958-08-09', 161);
+--INSERT INTO sanatci VALUES ('Mustafa', 'Yýldýzdoðan', 'Arabesk', '1966-05-10', 175);
+--INSERT INTO sanatci VALUES ('Neþet', 'Ertaþ', 'Türk Halk', '1938-01-01', 175);
+--INSERT INTO sanatci VALUES ('Yasemin', 'Yýldýz', 'Türk Halk', '1984-01-01', 165);
+--INSERT INTO sanatci VALUES ('Ekin', 'Uzunlar', 'Arabesk', '1991-08-21', 180);
+--INSERT INTO sanatci VALUES ('Kahraman', 'Deniz', 'Pop', '1990-08-19', 187);
+--INSERT INTO sanatci VALUES ('Burak', 'Bulut', 'Rap', '1980-09-26', 176);
+--INSERT INTO sanatci VALUES ('Aliþan Serkan', 'Tektaþ', 'Arabesk', '1976-06-19', 178);
+--INSERT INTO sanatci VALUES ('Aykut', 'Kuþkaya', 'Ýlahi', '1967-01-01', 175);
+--INSERT INTO sanatci VALUES ('Orhan', 'Gül', 'Rap', '1990-09-04', 175);
+--INSERT INTO sanatci VALUES ('Simge', 'Saðýn', 'Pop', '1981-08-08', 158);
+--INSERT INTO sanatci VALUES ('Ebru', 'Gündeþ', 'Pop', '1974-10-12', 160);
+--INSERT INTO sanatci VALUES ('Gülben', 'Ergen', 'Pop', '1972-08-25', 166);
+--INSERT INTO sanatci VALUES ('Mehmet', 'Karakuþ', 'Ýlahi', '1965-01-01', 175);
+--INSERT INTO sanatci VALUES ('Koray', 'Avcý', 'Pop', '1990-01-01', 182);
+--INSERT INTO sanatci VALUES ('Emircan', 'Ýðrek', 'Pop', '1993-04-02', 183);
+--INSERT INTO sanatci VALUES ('Ferman', 'Akgül', 'Pop', '1979-12-25', 180);
+
+--INSERT INTO muzikTuru VALUES ('Pop', 40, '20-35');
+--INSERT INTO muzikTuru VALUES ('Arabesk', 20, '25-50');
+--INSERT INTO muzikTuru VALUES ('Türk Halk', 20, '20-40');
+--INSERT INTO muzikTuru VALUES ('Rap', 10, '15-25');
+--INSERT INTO muzikTuru VALUES ('Ýlahi', 10, '35-50');
+
+--INSERT INTO yasAraligi VALUES ('20-35', 'Pop', 'Marmara Bölgesi', '45/55');
+--INSERT INTO yasAraligi VALUES ('25-50', 'Arabesk', 'Marmara Bölgesi', '65/35');
+--INSERT INTO yasAraligi VALUES ('20-40', 'Türk Halk', 'Karadeniz Bölgesi', '60/40');
+--INSERT INTO yasAraligi VALUES ('15-25', 'Rap', 'Ege Bölgesi', '60/40');
+--INSERT INTO yasAraligi VALUES ('35-50', 'Ýlahi', 'Ýç Anadolu Bölgesi', '50/50');
+
+
